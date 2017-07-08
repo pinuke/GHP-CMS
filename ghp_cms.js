52ac8be4f96f96f44d9ad7b63f2857f41179cec5
Nicholas Jackson <smartguy1196@gmail.com>
2017-07-07 18:56:52 -0700

    Update ghp_cms.js

--- ghp_cms.js
+++ ghp_cms.js
@@ -1,22 +1,21 @@
-function ghp( type, options )
+function ghp_get( owner, repo, callback, options )
 {
-  
-  this.fetchers = {
-    "ghp" : {
-    },
-    "twitter" : {},
-    "facebook" : {},
-    "instagram" : {},
-    "google+" : {},
-    "youtube" : {},
-    "pinterest" : {},
-    "giphy" : {},
-    "imgur" : {}
+  var init = {
+    "labels" : "blogpost"
   }
-  for(var index in extensions) { 
-   if (extensions.hasOwnProperty(index)) {
-       var value = extensions[index];
-       // do something with object value here.
-   }
+  if( ( typeof options.labels === "string" || options.labels instanceof String ) && options.labels.length > 0 ){
+    if( options.concatenate.labels === false ){
+      init.labels = options.labels;
+    }else{
+      init.labels = init.labels + "," + options.labels;
+    }
   }
+  var HTTP_req = new XMLHttpRequest();
+  HTTP_req.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      return this.responseText;
+    }
+  };
+  HTTP_req.open("GET", "http://api.github.com/repos/" + owner + "/" + repo + "/issues?labels=" + init.labels, true);
+  HTTP_req.send();
 }
