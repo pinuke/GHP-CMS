c3457039e0281e51e73a98d0ed465cb63a02070b
Nicholas Jackson <smartguy1196@gmail.com>
2017-07-07 20:55:25 -0700

    Fix bugs

--- ghp_cms.js
+++ ghp_cms.js
@@ -3,19 +3,21 @@ function ghp_get( owner, repo, callback, options )
   var init = {
     "labels" : "blogpost"
   }
-  if( ( typeof options.labels === "string" || options.labels instanceof String ) && options.labels.length > 0 ){
-    if( options.concatenate.labels === false ){
-      init.labels = options.labels;
-    }else{
-      init.labels = init.labels + "," + options.labels;
+  if( options ){
+  	if( ( typeof options.labels === "string" || options.labels instanceof String ) && options.labels.length > 0 ){
+      if( options.concatenate.labels === false ){
+        init.labels = options.labels;
+      }else{
+        init.labels = init.labels + "," + options.labels;
+      }
     }
   }
   var HTTP_req = new XMLHttpRequest();
   HTTP_req.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      return this.responseText;
+      callback( this.responseText );
     }
   };
-  HTTP_req.open("GET", "http://api.github.com/repos/" + owner + "/" + repo + "/issues?labels=" + init.labels, true);
+  HTTP_req.open("GET", "https://api.github.com/repos/" + owner + "/" + repo + "/issues?labels=" + init.labels, true);
   HTTP_req.send();
 }
