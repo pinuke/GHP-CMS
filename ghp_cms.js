e1a09474633757d38829e01395c89ee6ab09529c
Nicholas Jackson <smartguy1196@gmail.com>
2017-07-07 17:16:42 -0700

    Update ghp_cms.js

--- ghp_cms.js
+++ ghp_cms.js
@@ -1,4 +1,22 @@
-function ghp( )
+function ghp( type, options )
 {
-
+  
+  this.fetchers = {
+    "ghp" : {
+    },
+    "twitter" : {},
+    "facebook" : {},
+    "instagram" : {},
+    "google+" : {},
+    "youtube" : {},
+    "pinterest" : {},
+    "giphy" : {},
+    "imgur" : {}
+  }
+  for(var index in extensions) { 
+   if (extensions.hasOwnProperty(index)) {
+       var value = extensions[index];
+       // do something with object value here.
+   }
+  }
 }
